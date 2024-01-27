04e5277dc1f5ebd5eaa2c432713e25e1bbee526d
yuyanfenlin <97441166+yuyanfenlin@users.noreply.github.com>
2024-01-27 14:49:13 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="a2b59de5-fb67-4bf6-af18-6cf129dcf71e"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
